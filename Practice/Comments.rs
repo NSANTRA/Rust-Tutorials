8fb5d71a70e5d8bf3070f6eb63305d6405a4d4c6
NSANTRA <neelotpal.santra@gmail.com>
2025-09-10 20:15:52 +0530

    Additional Files

---
+++ Practice/Comments.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // This is a one line comment
+    /*
+    1. This is 
+    2. a multi line comment
+    */
+
+    println!("This is a comments program")
+}
---
+++ Practice/Format-Print.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // Type 1
+    println!("My number is {}", 10);
+    println!();
+
+    // Type 2
+    println!("{0}, this is {1}.\n{1}, this is {0}", "Jason", "Percy");
+    println!();
+    
+    // Type 3
+    println!("First Name: {first_name}, Last Name: {last_name}, Subject: {subject}", first_name = "Neelotpal", last_name = "Santra", subject = "Rust");
+    println!();
+
+    // Type 4
+    println!("Base 10: {}", 20);
+    println!("Base 2 (binary): {:b}", 20);
+    println!("Base 8 (octal): {:o}", 20);
+    println!("Base 16 (hexadecimal): {:x}", 20);
+    println!();
+
+    // Type 5
+
+}
