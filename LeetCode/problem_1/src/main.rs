d2cfc7c553746710a8aa2d50cecbb70c05042da3
NSANTRA <neelotpal.santra@gmail.com>
2025-09-23 12:55:00 +0530

    Completed LeetCode Problem 1

--- LeetCode/problem_1/src/main.rs
+++ LeetCode/problem_1/src/main.rs
@@ -1,3 +1,37 @@
+#[warn(dead_code)]
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    for i in 0..nums.len()-1 {
+        let a = target - &nums[i];
+
+        for j in i+1..nums.len() {
+            if a == nums[j] {
+                return vec![i as i32, j as i32];
+            }
+        }
+    }
+    vec![]
+}
+
 fn main() {
-    
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_1 () {
+        assert_eq!(vec![0, 1], two_sum(vec![2, 7, 11, 15], 9))
+    }
+
+    #[test]
+    fn case_2 () {
+        assert_eq!(vec![1, 2], two_sum(vec![3, 2, 4], 6))
+    }
+
+    #[test]
+    fn case_3 () {
+        assert_eq!(vec![0, 1], two_sum(vec![3, 3], 6))
+    }
 }
